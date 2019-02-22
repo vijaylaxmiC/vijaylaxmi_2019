8927e8e83c44a20a62f34c63959888265f24553a
Vidhyadhar <vidhyadhar.lonarmath@amadeus.com>
2019-02-22 21:22:28 +0530

    header on awards

--- YogeshWebsite/js/custom.js
+++ YogeshWebsite/js/custom.js
@@ -1,12 +1,12 @@
 var awardsText =  {
-	'awards/a1.jpg':"<h4>A meaningful salutations through dance : Prajavani news paper - Nanjundrao</h4>",
-	'awards/a2.jpg':"<h4>'Nauka Charitam' directed by Guru Padmini Ravi</h4>",
-	'awards/a3.jpg':"<h4>A show that enhances your love for Indian culture</h4>",
-	'awards/a4.jpg':"<h4>'Krishnapremi' - a dance fest which opened the doors of experimentations</h4>",
-	'awards/a5.jpg':"<h4>A strong proclamation of 'Guru Shisya parampara'</h4>",
-	'awards/a6.jpg':"<h4> Bharatanatyam exponent Pratibha Prahlad leaves audience and thrall</h4>",
-	'awards/a7.jpg':"<h4>Set the stage on fire through his dynamic bharatanatyam for the theme 'Agni' - Subbu Kriahna</h4>",	
-	'awards/a8.jpg':"<h4>Performed  at Koviayil thiruvaiyaru festival</h4>",
+	'awards/a1.jpg':"A meaningful salutations through dance : Prajavani news paper - Nanjundrao",
+	'awards/a2.jpg':"'Nauka Charitam' directed by Guru Padmini Ravi",
+	'awards/a3.jpg':"A show that enhances your love for Indian culture",
+	'awards/a4.jpg':"'Krishnapremi' - a dance fest which opened the doors of experimentations",
+	'awards/a5.jpg':"A strong proclamation of 'Guru Shisya parampara'",
+	'awards/a6.jpg':"Bharatanatyam exponent Pratibha Prahlad leaves audience and thrall",
+	'awards/a7.jpg':"Set the stage on fire through his dynamic bharatanatyam for the theme 'Agni' - Subbu Kriahna",	
+	'awards/a8.jpg':"Performed  at Koviayil thiruvaiyaru festival",
 }
 
 function createGallery(){
@@ -44,7 +44,9 @@ function createAwards(){
 	for (var i=1; i<=8; i++) {
 		var photoDiv = document.createElement('div')
 		photoDiv.className = "gallery_image_div"
-		var imgHTML = '<img class= "gallery-img" src="awards/a'+i+'.jpg" onclick="AwardsImageClicked(this)">';
+		var imgHeading = 'awards/a'+i+'.jpg'
+		var imgHTML = '<p class ="award_heading">'+awardsText[imgHeading]+'</p>'
+		imgHTML += '<img class= "gallery-img" src="awards/a'+i+'.jpg" onclick="AwardsImageClicked(this)">';
 		photoDiv.innerHTML = imgHTML;
 		parentElement.appendChild(photoDiv)
 	}
